7509f8e5b3c5ad0ceb51f2309c47d10aaaad9ef8
Nic Crane <thisisnic@gmail.com>
2023-02-01 22:58:21 +0000

    Implement time formatting function

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,31 @@
 #include <string>
+#include <math.h>       
+
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    
+  long remaining = seconds;
+  long hours = floor(remaining/(60*60));
+  remaining -= hours*60*60;
+  long minutes = floor(remaining/(60));
+  remaining -= minutes*60;
+
+  std::string mins = std::to_string(minutes);
+  if(mins.length() == 1){
+    mins.insert(0, 1, '0');
+  }
+
+  std::string secs = std::to_string(remaining);
+  if(secs.length() == 1){
+    secs.insert(0, 1, '0');
+  }
+  
+  std::string hms = std::to_string(hours) + ":" + mins  + ":" + secs;
+
+  return hms;
+
+}
