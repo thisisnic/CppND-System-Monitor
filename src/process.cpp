7c773b07e5875c7faabcf07c62998eb9bcb8190d
Nic Crane <thisisnic@gmail.com>
2023-02-05 22:25:50 +0000

    Add constructor for process

--- src/process.cpp
+++ src/process.cpp
@@ -5,25 +5,32 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
+Process::Process(int pid){
+    pid_ = pid;
+    command_ = LinuxParser::Command(pid);
+    user_ = LinuxParser::User(pid);
+}
+
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { return 0; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { return command_; }
 
 // TODO: Return this process's memory utilization
 string Process::Ram() { return string(); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { return user_; }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { return 0; }
