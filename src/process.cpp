d9c42750f96be0e4aae5bcdc902f72aed6c2c40a
Nic Crane <thisisnic@gmail.com>
2023-02-21 23:52:26 +0000

    Calculate uptime by deducting from total

--- src/process.cpp
+++ src/process.cpp
@@ -31,7 +31,7 @@ string Process::User() { return user_; }
 
 // TODO: Return the age of this process (in seconds)
 long int Process::UpTime() {
-    return LinuxParser::UpTime(pid_);
+    return LinuxParser::UpTime() - LinuxParser::UpTime(pid_);
 }
 
 // TODO: Overload the "less than" comparison operator for Process objects
